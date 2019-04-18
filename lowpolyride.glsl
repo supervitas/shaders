451baaa7c2008805381fec365d9ba4d916ad60f3
Vitaly Nikolaev <nikolaev.vitally@yandex.ru>
2019-04-19 02:24:08 +0300

    Update lowpolyride.glsl

--- lowpolyride.glsl
+++ lowpolyride.glsl
@@ -15,10 +15,10 @@ precision highp float;
 #define TRUNK vec3(0.175,0.050,0.005)
 #define CAR_TIRES vec3(0.060,0.060,0.060)
 #define ROAD vec3(0.150,0.150,0.150)
+#define CAR_WINDOW vec3(0.505,0.540,0.510)
 #define ROAD_WIDTH 12.752
 #define TREES_ROAD_OFFSET_RIGHT ROAD_WIDTH + 2.
 #define SPEED 26.
-
 
 uniform vec2 u_resolution;
 uniform vec2 u_mouse;
@@ -172,12 +172,12 @@ vec4 createCar(vec3 p) {
     car.x = opSubtraction(subFront, car.x);
     car.x = opSubtraction(subBack, car.x);
     
-    vec4 windowBack =  vec4(sdBox(p + vec3(0., -3. - jumping, 2.3), vec3(1.3, .43, 0.01)) - 0.3, vec3(0.505,0.540,0.510));
-    vec4 windowLeft =  vec4(sdBox(rotateY(-1.548) * p + vec3(0., -3. - jumping, 1.8), vec3(1.3, .43, 0.01)) - 0.3, vec3(0.505,0.540,0.510));
+    vec4 windowBack =  vec4(sdBox(p + vec3(0., -3. - jumping, 2.3), vec3(1.3, .43, 0.01)) - 0.21, CAR_WINDOW);
+    vec4 windowLeft =  vec4(sdBox(rotateY(-1.548) * p + vec3(0., -3. - jumping, 1.8), vec3(1.3, .43, 0.01)) - 0.3, CAR_WINDOW);
     car = unionSDF(car, windowBack);
     car = unionSDF(car, windowLeft);
 
-    vec3 t =  rotateZ(1.564) * p;
+    vec3 t = rotateZ(1.564) * p;
    
     vec3 wheelBackPosition = t + vec3(-0.2 - jumping * .5, .4 , 2.1);
     vec3 wheelFrontPosition = t + vec3(-0.2 - jumping * .5, .4, -2.1);
@@ -223,7 +223,6 @@ vec4 map(vec3 samplePoint) {
     * step( mod(samplePoint.z + SPEED * u_time , 16.), 6.6); // getting offset
     
     vec4 plane = vec4(sdPlane(samplePoint), mix(ROAD, vec3(1.0), sizeOfLine));
-    
 
     float insideRoad = step(-ROAD_WIDTH, samplePoint.x) * step(samplePoint.x, ROAD_WIDTH) ;
     vec4 trees = vec4(1.);
@@ -267,7 +266,6 @@ vec3 getNormal(vec3 p) {
     ));
 }
 
-
 float calcSoftshadow(in vec3 ro, in vec3 rd) {
 	float res = 1.0;
     float t = 0.01;
@@ -280,53 +278,34 @@ float calcSoftshadow(in vec3 ro, in vec3 rd) {
     return clamp( res, 0.0, 1.0 );
 }
 
-
-vec3 phongContribForLight(vec3 k_d, vec3 k_s, float alpha, vec3 p, vec3 eye, vec3 lightPos, vec3 lightIntensity) {
-    vec3 N = getNormal(p);
-    vec3 L = normalize(lightPos - p);
-    vec3 V = normalize(eye - p);
-    vec3 R = normalize(reflect(-L, N));
-    
-    float dotLN = dot(L, N);
-    float dotRV = dot(R, V);
-    
-    if (dotLN < 0.0) { // Light not visible from this point on the surface
-        return vec3(0.0, 0.0, 0.0);
-    } 
-    
-    if (dotRV < 0.0) { // Light reflection in opposite direction as viewer, apply only diffuse component
-        return lightIntensity * (k_d * dotLN);
-    }
-    
-    return lightIntensity * (k_d * dotLN + k_s * pow(dotRV, alpha));
+vec3 fresnel( vec3 F0, vec3 h, vec3 l ) {
+	return F0 + ( 1.0 - F0 ) * pow( clamp( 1.0 - dot( h, l ), 0.0, 1.0 ), 5.0 );
 }
 
+vec3 phongIllumination(vec3 p, vec3 dir) { 
+    vec3 Ks = vec3(0.425,0.425,0.425);
+    vec3 Kd = vec3(5.5);
+  	vec3 n = getNormal(p);
+    	
+	vec3 ref = reflect( dir, n );
 
-vec3 phongIllumination(vec3 k_a, vec3 k_d, vec3 k_s, float alpha, vec3 p, vec3 eye, vec3 lp) {
-    const vec3 ambientLight = .8 * vec3(1.0, 1.0, 1.0);
-    vec3 color = ambientLight * k_a;
-    
-    vec3 light1Pos = lp;
-    vec3 light1Intensity = vec3(1.0);
-    
-    color += phongContribForLight(k_d, k_s, alpha, p, eye,light1Pos,light1Intensity);
+    vec3 light_pos  = vec3( 100.0, 200.0, 40.040 );
+	vec3 light_color = vec3(0.995,0.900,0.872);
+    const float shininess = 1.6;
+	
+	vec3 vl = normalize( light_pos - p );
+	
+	vec3 diffuse  = Kd * vec3( max( 0.0, dot( vl, n ) ) );
+	vec3 specular = vec3( max( 0.0, dot( vl, ref ) ) );
+		
+    vec3 F = fresnel( Ks, normalize( vl - dir ), vl );
+	specular = pow( specular, vec3( shininess ) );
+		
+	vec3 color = light_color * mix( diffuse, specular, F ) + light_color; 
       
     return color;
 }
             
-
-vec3 rayDirection(float fieldOfView, vec2 size, vec2 fragCoord) {
-    vec2 xy = fragCoord - size / 2.0;
-    float z = size.y / tan(radians(fieldOfView) / 2.0);
-    return normalize(vec3(xy, -z));
-}
-
-mat3 viewMatrix(vec3 eye, vec3 center, vec3 up) {
-    vec3 f = normalize(center - eye);
-    vec3 s = normalize(cross(f, up));
-    vec3 u = cross(s, f);
-    return mat3(s, u, -f);
-}
 
 mat3 calcLookAtMatrix(vec3 origin, vec3 target, float roll) {
   vec3 rr = vec3(sin(roll), cos(roll), 0.0);
@@ -336,7 +315,6 @@ mat3 calcLookAtMatrix(vec3 origin, vec3 target, float roll) {
 
   return mat3(uu, vv, ww);
 }
-
 
 float calcAO( vec3 pos, vec3 nor ) {
 	float occ = 0.0;
@@ -352,26 +330,22 @@ float calcAO( vec3 pos, vec3 nor ) {
 }
 
 vec3 render(vec2 p, vec2 uv) {
-
     vec3 ro = vec3(6., 22., -13.6);
+     // vec3 ro = mix(vec3(4., 21., -11.6), vec3(6., 22., -13.6), sin(u_time ));
     
-    vec3 ta =  normalize(vec3(0.,0.,-1.000));
+    vec3 ta =  normalize(vec3(0.320,0.500,-1.000));
     mat3 ca = calcLookAtMatrix(ro, ta, 0.0);
     vec3 rd = ca * normalize(vec3(p.xy, 1.2));
     
     vec4 scene = raymarsh(ro, rd, MIN_DIST, MAX_DIST);
  	vec3 point = ro + scene.x * rd;
-     vec3 nor = getNormal(point);
+    vec3 nor = getNormal(point);
 
 
-    float shininess = 5.824;
-
-    vec3 lightPosition =  vec3(-10.0, 20.0, 30.);
-    scene.yzw *= phongIllumination(vec3(1.8), vec3(2.5), vec3(1.100,0.893,0.064), shininess, point ,  ro, lightPosition);
-    // color *= vec3(2.5);
-    // color *= calcSoftshadow(p, vec3(0.3,3 , 1.));
-    // color *= calcAO( p, nor );
-
+    scene.yzw *= phongIllumination(point, rd);
+    // scene.yzw *= vec3(1.5);
+    // scene.yzw *= calcSoftshadow(point, ro);
+    // scene.yzw *= calcAO( point, nor );
     
 	return scene.yzw;
 }
@@ -394,7 +368,6 @@ void main() {
 #endif 
  
     color *= 0.25+0.334*pow( 16.0 * uv.x * uv.y * (1.0 - uv.x) * (1.0 - uv.y), 0.3 ); // Vigneting
-	// color = pow(color, vec3(1. / 2.2)); // gamma correction
     color = smoothstep(0., .7, color);
     gl_FragColor = vec4(color, 1.0);
 }
